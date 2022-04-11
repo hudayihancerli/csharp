4f203d87b90260324f59df43f04d6478243131c5
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-11 23:18:24 +0300

    Add files via upload

---
+++ csharp3/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] rakamlar = new int[9];
+
+string[] hayvanlar = {"kedi", "köpek", "kedi balığı", "köpekbalığı"};
+
+rakamlar[0] = 8;
+hayvanlar[0] = "maymun";
+
+foreach (var rakam in rakamlar)
+{
+    Console.WriteLine(rakam);
+}
+
+
+Console.WriteLine("Lütfen bir sayı giriniz");
+int number =Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[number];
+for(int i = 0; i < number; i++){
+    Console.Write($"{i+1}. sayıyı giriniz : ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+foreach (var num in arr)
+{
+    
+    Console.WriteLine(num);
+}
