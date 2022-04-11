f5c27e386ba49fca319cb8dd65da5404ef4969a8
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-11 10:05:13 +0300

    Add files via upload

---
+++ csharp1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz.");
+int a =Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<a; i++)
+{
+    if(i%2==0){
+        Console.WriteLine($"{i} sayısı çifttir.");
+    }else{
+        Console.WriteLine($"{i} sayısı tektir.");
+    }
+}
+  int tek = 0, cift = 0;
+for(int j = 0; j<=1000; j++){
+  
+
+    if(j%2==0){
+        cift += j;
+    }else{
+        tek += j;
+    }
+
+    
+}Console.WriteLine($"Tek toplam : {tek}");
+    Console.WriteLine($"Çift toplam : {cift}");
