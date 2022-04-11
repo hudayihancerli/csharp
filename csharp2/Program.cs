ebc99239b4a312821513a03643612d0eccd2304b
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-11 10:20:55 +0300

    Add files via upload

---
+++ csharp2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz.");
+int sayi =Convert.ToInt32(Console.ReadLine());
+int toplam = 0;
+int sayac = 1;
+while(sayac <= sayi){
+    toplam += sayac;
+    sayac++;
+}
+Console.WriteLine($"toplam = {toplam}, ortalama = {toplam/sayi}");
+
+char alf = 'a';
+while(alf < 'z'){
+    Console.WriteLine(alf);
+    alf++;
+}
+
+string[] harfler = {"a","b","c","d","e"};
+
+foreach (var harf in harfler)
+{
+    Console.WriteLine(harf);
+}
+
