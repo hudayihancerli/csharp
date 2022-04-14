71c26b044ebd433797cf28c6d52b3e7204b3993e
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-15 00:47:18 +0300

    Add files via upload

---
+++ csharp8/Program.cs
@@ -0,0 +1,53 @@
+﻿string degisken =  "ders csharp";
+string degisken2 =  "ders";
+
+//lenght
+Console.WriteLine(degisken.Length);
+
+//ToUpper - ToLower
+Console.WriteLine(degisken.ToLower());
+Console.WriteLine(degisken.ToUpper());
+
+//Concant
+Console.WriteLine(String.Concat(degisken," Merhaba"));
+
+//Compare, CompareTo -1, 0, 1
+Console.WriteLine(degisken.CompareTo(degisken2));
+Console.WriteLine(string.Compare(degisken, degisken2, true));
+Console.WriteLine(string.Compare(degisken, degisken2, false));
+
+//contains
+Console.WriteLine(degisken.Contains(degisken2));
+Console.WriteLine(degisken.EndsWith("."));
+Console.WriteLine(degisken.StartsWith("m"));
+
+//IndexOf
+Console.WriteLine(degisken.IndexOf("cs"));
+Console.WriteLine(degisken.IndexOf("fsdf"));
+Console.WriteLine(degisken.LastIndexOf("i"));
+
+//Insert
+Console.WriteLine(degisken.Insert(0,"ekle"));
+
+//padleft, padright
+Console.WriteLine(degisken + degisken2.PadLeft(30));
+Console.WriteLine(degisken + degisken2.PadLeft(30,'*'));
+Console.WriteLine(degisken + degisken2.PadRight(30));
+Console.WriteLine(degisken + degisken2.PadRight(30,'*'));
+
+//remove
+Console.WriteLine(degisken.Remove(10));
+Console.WriteLine(degisken.Remove(5,3));
+Console.WriteLine(degisken.Remove(0,1));
+
+//replace
+Console.WriteLine(degisken.Replace("csharp","c#"));
+Console.WriteLine(degisken.Replace(" ","*"));
+
+//split
+Console.WriteLine(degisken.Split(' ')[1]);
+
+//substring
+Console.WriteLine(degisken.Substring(4));
+Console.WriteLine(degisken.Substring(4,6));
+
