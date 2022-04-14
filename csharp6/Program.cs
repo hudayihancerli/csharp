de69eacde490a1cd43a2fa62ce0cc47f78818776
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-14 22:02:07 +0300

    Add files via upload

---
+++ csharp6/Program.cs
@@ -0,0 +1,35 @@
+﻿string sayi = "123";
+
+bool sonuc = int.TryParse(sayi, out int outSayi);
+
+if(sonuc){
+    Console.WriteLine("Başarılı");
+    Console.WriteLine(outSayi);
+}else{
+    Console.WriteLine("Başarısız");
+}
+
+Metotlar instance = new Metotlar();
+instance.Topla(4, 5, out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+
+int ifade = 123;
+instance.EkranaYazdir(Convert.ToString(ifade));
+instance.EkranaYazdir(ifade);
+instance.EkranaYazdir("a", "b");
+
+class Metotlar{
+    public void Topla(int a, int b, out int toplam){
+        toplam = a+b;
+    }
+
+    public void EkranaYazdir(string veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(string veri1, string veri2){
+        Console.WriteLine(veri1 + veri2);
+    }
+}
