16d740b139dd39fd3a8579b3bc56e0028edf3633
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-15 23:33:23 +0300

    Add files via upload

---
+++ csharp12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+//dictionary
+
+Dictionary<int,string> kullacilar = new Dictionary<int, string>();
+
+kullacilar.Add(10,"on");//key, value
+kullacilar.Add(12,"oniki");
+kullacilar.Add(18,"onsekiz");
+kullacilar.Add(20,"yirmi");
+
+//dizinin elemanlarına erişim
+Console.WriteLine(kullacilar[12]);
+foreach(var item in kullacilar){
+    Console.WriteLine(item); //item.value
+}
+
+//count
+Console.WriteLine(kullacilar.Count);
+
+//Contains
+Console.WriteLine(kullacilar.ContainsKey(12));//var mı?
+Console.WriteLine(kullacilar.ContainsValue("yok"));//var mı?
+
+
