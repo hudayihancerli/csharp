d820b6c3c1fc6ce01e588f34bc1dd7e83e6b164d
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-04-17 21:11:04 +0300

    Add files via upload

---
+++ csharp16/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine(Calisan.CalisanSayisi);
+Calisan calisan1 = new Calisan("a","b");
+Console.WriteLine(Calisan.CalisanSayisi);
+
+
+
+class Calisan{
+    private static int calisanSayisi;
+
+    public static int CalisanSayisi {get => calisanSayisi;}
+
+    private string ad;
+    private string soyad;
+
+    static Calisan(){
+        calisanSayisi = 0;
+    }
+
+    public Calisan(string ad, string soyad){
+        this.ad = ad;
+        this.soyad = ad;
+        calisanSayisi++;
+    }
+}
